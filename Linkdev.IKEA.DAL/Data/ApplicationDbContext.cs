27a189525821e2a78c9708d23e1fd154203a2987
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-09-14 17:09:02 +0300

    3-Tiers Archetichure pattern - Data Access Layer

---
+++ Linkdev.IKEA.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linkdev.IKEA.DAL.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext():base()
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder.UseSqlServer("Server = .; Database=MvcApplication; Trusted_connection = true; TrustServerCertificate = true;");
+	}
+}
